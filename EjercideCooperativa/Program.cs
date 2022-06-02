611cd858321e2f93ce69156b791ae5498529ed41
Estudiante <Estudiante@MLB09>
2022-06-02 14:49:49 -0600

    Agregar archivos de proyecto.

---
+++ EjercideCooperativa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercideCooperativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            int v, t, d, autobus;
+            Console.Write(" Escriba el nombre de la ruta: ");
+            autobus = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese velocidad:");
+            v = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese distancia:");
+            d = int.Parse(Console.ReadLine());
+
+            t = v * d;
+
+            Console.WriteLine("El tiempo es = {0}", t);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
